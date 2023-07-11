6d1c3ff71053512419eed8695e990b2bfcf01d1b
kittinan.boonyaratprapan@stream.co.th <kittinan.boonyaratprapan@stream.co.th>
2023-07-11 10:56:20 +0700

    init

---
+++ src/main/java/com/example/mockupResponse/MockupResponseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockupResponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockupResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockupResponseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mockupResponse/MockupResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockupResponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockupResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
